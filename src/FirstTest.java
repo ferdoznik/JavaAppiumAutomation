4de202bc0b27023aa3c7e6758ebeac9d3bf421eb
ferdoznik <64578728+ferdoznik@users.noreply.github.com>
2023-03-05 15:16:16 +0300

    Fist

---
+++ src/FirstTest.java
@@ -0,0 +1,36 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstTest {
+    private AppiumDriver driver;
+    @Before
+    public void setUp() throws Exception
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName","AndroidTestDevice");
+        capabilities.setCapability("platformName","13.0");
+        capabilities.setCapability("automationName","Appium");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".maim.MainActivity");
+        capabilities.setCapability("app","/Users/antonragozin/Desktop/JavaAppiumAutomation/JavaAppiumAutomation/apks");
+
+        driver = new AndroidDriver(new URL("0.0.0.0:4723/wd/hub"), capabilities);
+    }
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+    @Test
+    public void firstTest()
+    {
+        System.out.println("First test run");
+    }
+}
