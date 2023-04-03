9299edfc1cb2ecca5a4a12db4765e5205eff1344
ferdoznik <64578728+ferdoznik@users.noreply.github.com>
2023-04-03 21:21:11 +0300

    lesson6

---
+++ src/Tests/iOS/GetStartedTest.java
@@ -0,0 +1,25 @@
+package Tests.iOS;
+
+import lib.iOSTestCase;
+import lib.ui.WelcomePageObject;
+import org.junit.Test;
+
+public class GetStartedTest extends iOSTestCase
+{
+    @Test
+    public void testPassThroughWelcome()
+    {
+        WelcomePageObject WelcomePage = new WelcomePageObject(driver);
+
+        WelcomePage.waitForLearnMoreLink();
+        WelcomePage.clickNextButton();
+
+        WelcomePage.waitForNewWaysToExplore();
+        WelcomePage.clickNextButtonSecondTime();
+
+        WelcomePage.waitForSearchInNearly300Languages();
+        WelcomePage.clickGetStarted();
+
+        WelcomePage.clickGetStarted();
+    }
+}
