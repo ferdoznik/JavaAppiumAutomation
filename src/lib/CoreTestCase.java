1ae105ebf6579178f893633b25f6ad279be8ce59
ferdoznik <64578728+ferdoznik@users.noreply.github.com>
2023-04-05 22:26:16 +0300

    HW_L6

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -1,27 +1,21 @@
 package lib;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import java.net.URL;
 import java.time.Duration;
 
 public class CoreTestCase extends TestCase{
 
-    private static final String PLATFORM_IOS = "ios";
-    private static final String PLATFORM_ANDROID = "android";
-
     protected AppiumDriver driver;
-    private static String AppiumURL = "http://0.0.0.0:4723/wd/hub";
+    protected Platform Platform;
 
     @Override
     protected void setUp() throws Exception
     {
         super.setUp();
-        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
-        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        this.Platform = new Platform();
+        driver = this.Platform.getDriver();
         this.rotateScreenPortrait();
     }
 
@@ -46,30 +40,5 @@ public class CoreTestCase extends TestCase{
     protected void BackgroundApp(int seconds)
     {
         driver.runAppInBackground(Duration.ofSeconds(seconds));
-    }
-    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception
-    {
-        String platform = System.getenv("PLATFORM");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        if (platform.equals(PLATFORM_ANDROID)) {
-            capabilities.setCapability("platformName", "Android");
-            capabilities.setCapability("deviceName", "AndroidTestDevice");
-            capabilities.setCapability("platformName", "13.0");
-            capabilities.setCapability("automationName", "Appium");
-            capabilities.setCapability("appPackage", "org.wikipedia");
-            capabilities.setCapability("appActivity", ".main.MainActivity");
-            capabilities.setCapability("app", "/Users/antonragozin/Desktop/JavaAppiumAutomation/JavaAppiumAutomation/apks/org.wikipedia.apk");
-
-        } else if (platform.equals(PLATFORM_IOS)) {
-            capabilities.setCapability("platformName", "iOS");
-            capabilities.setCapability("platformVersion", "15.0");
-            capabilities.setCapability("deviceName", "iPhone 11");
-            capabilities.setCapability("automationName", "XCUITest");
-            capabilities.setCapability("app", "/Users/antonragozin/Desktop/JavaAppiumAutomation/JavaAppiumAutomation/apks/Wikipedia.app");
-        } else {
-            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
-        }
-        return capabilities;
     }
 }
